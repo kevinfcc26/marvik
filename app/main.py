de2c297af3663ca4d75a9605b945e7bc725214ff
kevinfcc26 <kevinfcc26@gmail.com>
2024-05-31 14:36:08 -0500

    feat: add user routes to main app

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.routing import APIRoute
 from starlette.middleware.cors import CORSMiddleware
 
+from app.api.main import api_router
 from app.core.config import settings
 
 
@@ -24,3 +25,5 @@ if settings.BACKEND_CORS_ORIGINS:
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
